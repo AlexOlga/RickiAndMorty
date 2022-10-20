ed8e49875666811e2b28cd7691ef966f140dac89
AlexOlga <bulbenko@yandex.ru>
2022-10-20 10:17:31 +0300

    feat: Cards component rewrite whith the hooks

--- react-components/src/components/cards/cards.tsx
+++ react-components/src/components/cards/cards.tsx
@@ -1,15 +1,42 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { CardsProps, ICharacter } from '../../types';
 import './cards.css';
 import Card from '../card/card';
 import Modal from '../modal/modal';
 
-type CardsState = {
+/*type CardsState = {
   isOpen: boolean;
   dataCard: ICharacter;
+}; */
+const Cards = (cardData: CardsProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [dataCard, setDataCard] = useState({});
+  const handleCancel = (e: React.MouseEvent) => {
+    const target = e.target as HTMLElement;
+    if (target.classList.value.includes('modal-overlay')) setIsOpen(false);
+    if (target.classList.value.includes('button-close')) setIsOpen(false);
+  };
+  const data = cardData.data; // передать просто data
+  return (
+    <>
+      <h1 className="title">Characters from the TV show Rick and Morty</h1>
+      <div className="cards-contener">
+        {data.map((item: ICharacter) => (
+          <Card
+            character={item}
+            key={item.id}
+            onClick={() => {
+              setDataCard(item);
+              setIsOpen(true);
+            }}
+          />
+        ))}
+      </div>
+      <Modal isOpen={isOpen} onCancel={(e: React.MouseEvent) => handleCancel(e)} data={dataCard} />
+    </>
+  );
 };
-
-class Cards extends Component<CardsProps, CardsState> {
+/*class Cards extends Component<CardsProps, CardsState> {
   state = {
     isOpen: false,
     dataCard: {},
@@ -44,6 +71,6 @@ class Cards extends Component<CardsProps, CardsState> {
       </>
     );
   }
-}
+}*/
 
 export default Cards;
