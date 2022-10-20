6f5a76b3669615506cbe61a47824b4d781f2a08d
AlexOlga <bulbenko@yandex.ru>
2022-10-20 10:25:50 +0300

    feat: modal component rewrite whith the hooks

--- react-components/src/components/modal/modal.tsx
+++ react-components/src/components/modal/modal.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { ICharacter } from 'types';
 import './modal.css';
 type ModalProps = {
@@ -6,8 +6,60 @@ type ModalProps = {
   onCancel: (e: React.MouseEvent) => void;
   data: ICharacter;
 };
+const Modal = (modalProps: ModalProps) => {
+  const { isOpen, onCancel, data } = modalProps;
+  useEffect(() => {
+    if (isOpen) {
+      document.body.classList.add('scroll');
+    } else {
+      document.body.classList.remove('scroll');
+    }
+  });
 
-class Modal extends Component<ModalProps> {
+  return (
+    <>
+      {isOpen && (
+        <div className="modal-overlay" onClick={onCancel}>
+          <div className="modal-window" id="modal">
+            <div className="modal-header">
+              <h3 className="character_name">{data.name}</h3>
+              <button
+                name="close"
+                onClick={onCancel}
+                className="button-close"
+                data-toggle="modal"
+                data-target="#test-modal"
+              >
+                X
+              </button>
+            </div>
+            <div className="modal-body">
+              <img className="character_img" src={data.image} alt="Character image" />
+              <div className="text-contener">
+                <p>
+                  <span className="character_category">Status: </span> {data.status}
+                </p>
+                <p>
+                  <span className="character_category">Species: </span>
+                  {data.species}
+                </p>
+                <p>
+                  <span className="character_category">Last known location: </span>
+                  {data.location?.name}
+                </p>
+                <p>
+                  <span className="character_category">First seen in: </span>
+                  {data.origin?.name}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+/*class Modal extends Component<ModalProps> {
   componentDidUpdate() {
     if (this.props.isOpen) {
       document.body.classList.add('scroll');
@@ -62,5 +114,5 @@ class Modal extends Component<ModalProps> {
       </>
     );
   }
-}
+}*/
 export default Modal;
